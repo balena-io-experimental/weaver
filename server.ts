d12dc8333c0062871cdf8f4c54351162b6c42ead
Matthew Yarmolinsky <yarmolinskymatthew@gmail.com>
2022-05-03 11:21:36 -0400

    Parse file content into nivo data nodes and links
    Change-type: minor
    Signed-off-by: Matthew Yarmolinsky <matthew-timothy@balena.io>

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const port = 8000;
 
 app.use(json());
 app.use(urlencoded());
-app.use(function (req, res, next) {
+app.use(function (req: any, res: any, next: any) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
@@ -19,14 +19,20 @@ app.use(function (req, res, next) {
   next();
 });
 app.disable("etag");
-app.post("/clone", async (req, res) => {
+app.get("/clean", async (req: any, res: any) => {
+  if (existsSync(REPO_PATH)) {
+    await promises.rm(REPO_PATH, { recursive: true });
+  }
+});
+
+app.post("/clone", async (req: any, res: any) => {
   if (existsSync(REPO_PATH)) {
     await promises.rm(REPO_PATH, { recursive: true });
   }
   cloneRepo(res, req.body.data);
 });
 
-app.get("/getRepoInfo", (req, res) => {
+app.get("/getRepoInfo", (req: any, res: any) => {
   getRepoInfo(res);
 });
 
@@ -41,7 +47,11 @@ const cloneRepo = async (res: any, repoUrl: string) => {
     return;
   }
   if (!existsSync(REPO_PATH)) {
-    await promises.mkdir(REPO_PATH);
+    try {
+      await promises.mkdir(REPO_PATH);
+    } catch (err) {
+      logError(err);
+    }
   }
   try {
     execSync(`cd ${REPO_PATH} && git clone ${repoUrl} && cd ..`);
@@ -52,32 +62,71 @@ const cloneRepo = async (res: any, repoUrl: string) => {
   }
 };
 
-interface NodeProps {
+export interface NodeProps {
   id: string;
   size: number;
 }
 
-interface LinkProps {
+export interface LinkProps {
   source: string;
   target: string;
+  distance: number;
 }
 
 const getRepoInfo = async (res: any) => {
+  const repoName = (await getDirectories(REPO_PATH))?.[0];
   let nodes: Array<NodeProps> = [];
   let links: Array<LinkProps> = [];
   const files = await getFilesInDirectory(REPO_PATH);
+
   if (!files) {
     return null;
   }
   for (const filePath of files) {
-    const fileContent = await promises.readFile(filePath, "utf8");
-    nodes.push({ id: filePath, size: 2 });
-    fileContent.split(/\r?\n/).forEach((line: string, lineIndex: number) => {
-      // testing nivo node creation
-      nodes.push({ id: filePath + lineIndex, size: 1 });
-      links.push({ source: filePath, target: filePath + lineIndex });
-    });
-    break;
+    const repoFilePath = filePath.replace(`repository/${repoName}/`, "");
+    try {
+      nodes.push({ id: `/${repoFilePath}`, size: 1 });
+      if (
+        !repoFilePath.endsWith("tsx") &&
+        !repoFilePath.endsWith("ts") &&
+        !repoFilePath.endsWith("jsx") &&
+        !repoFilePath.endsWith("js")
+      ) {
+        continue;
+      }
+      const fileContent = await promises.readFile(filePath, "utf8");
+      /**
+       * repoFilePath
+       * imports: `import X from 'x'`, `import {X, Y, Z} from 'x'`, `import * as X from './X/Y/Z'`
+       * consts/exports: `export const`, `const`, `interface`
+       */
+      const imports = getImports(fileContent);
+      for (const imp of imports) {
+        let relativePath = imp.path;
+        if (imp.path.startsWith(".")) {
+          relativePath = path.normalize(
+            `/${repoFilePath.replace(/(?!.*\/).+/, "")}${imp.path}`
+          );
+        }
+        for (const comp of imp.components) {
+          const idx = nodes.findIndex(
+            (node) => node.id === `${comp.name}|${relativePath}`
+          );
+          if (idx >= 0) {
+            nodes[idx].size = nodes[idx].size + 1;
+          } else {
+            nodes.push({ id: `${comp.name}|${relativePath}`, size: 1 });
+          }
+          links.push({
+            source: `/${repoFilePath}`,
+            target: `${comp.name}|${relativePath}`,
+            distance: 25,
+          });
+        }
+      }
+    } catch (err) {
+      logError(err);
+    }
   }
   // TODO: for now we just close the request
   res.status(200).json({ nodes, links });
@@ -89,18 +138,22 @@ const getFilesInDirectory = async (dir: string) => {
   }
   const files: string[] = [];
   const directory = await promises.readdir(dir);
-  for (const fileName of directory) {
-    const filePath = path.join(dir, fileName);
-    const stat = await promises.lstat(filePath);
-    if (stat.isDirectory()) {
-      const nestedFile = await getFilesInDirectory(filePath);
-      if (!nestedFile) {
-        return null;
+  try {
+    for (const fileName of directory) {
+      const filePath = path.join(dir, fileName);
+      const stat = await promises.lstat(filePath);
+      if (stat.isDirectory()) {
+        const nestedFile = await getFilesInDirectory(filePath);
+        if (!nestedFile) {
+          return null;
+        }
+        files.push(...nestedFile);
+      } else {
+        files.push(filePath);
       }
-      files.push(...nestedFile);
-    } else {
-      files.push(filePath);
     }
+  } catch (err) {
+    logError(err);
   }
   return files;
 };
@@ -117,4 +170,52 @@ const checkFileExists = async (dir: string) => {
 
 const logError = (...args: any) => {
   console.error("\x1b[31m%s\x1b[0m", ...args);
+};
+/**
+ * getImports
+ *
+ * @param fileContent
+ * @returns [{components: [{name: string, alias: string}], path: string}]
+ */
+const getImports = (fileContent: string) => {
+  return fileContent
+    .replaceAll(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, "")
+    .replaceAll("\\`", "")
+    .replaceAll(/`([\s\S]*?)`/gm, "")
+    .split("import")
+    .filter((x) => x.includes("from "))
+    .map((a) => {
+      const [components, path] = a.split("from");
+      return {
+        components: components
+          .replaceAll(/\r?\n/g, "")
+          .replaceAll("{", "")
+          .replaceAll("}", "")
+          .replaceAll("\t", "")
+          .split(",")
+          .filter((a) => !!a.trim())
+          .map((t) => {
+            const aliased = t.split(" as ");
+            return {
+              name: aliased[0].replaceAll(" ", ""),
+              alias: !!aliased[1] ? aliased[1].replaceAll(" ", "") : null,
+            };
+          }),
+        path: path
+          .replaceAll(";", "")
+          .replaceAll(" ", "")
+          .replaceAll('\\"', "")
+          .replaceAll("'", "")
+          .replaceAll('"', "")
+          .split(/\r?\n/)[0],
+      };
+    });
+};
+
+const getDirectories = async (path: string) => {
+  const files = await promises.readdir(path);
+  return files.filter(async (file) => {
+    const fileType = await promises.stat(path + "/" + file);
+    return fileType.isDirectory();
+  });
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-import { getRepo } from './utils/github';
-import { cloneRepo, getRepoInfo } from './utils/requests';
+import React from "react";
+import { useRequest, notifications, Spinner, Box } from "rendition";
+import { getRepo } from "./utils/github";
+import { clean, cloneRepo, getRepoInfo } from "./utils/requests";
+import { NodeProps, LinkProps } from "../server";
+import { ResponsiveNetwork } from "@nivo/network";
+// import data from "./utils/temp.json";
 
-function App() {
+const App = () => {
+  React.useEffect(() => {
+    return () => {
+      clean();
+    };
+  }, []);
+
   // TODO: Make this an input where the user can write repo owner and name
-  getRepo('balena-io-modules', 'rendition').then((repoInfo) => {
-    cloneRepo(repoInfo.data.clone_url)?.then((res) => {
-      getRepoInfo().then((data) => {
-        console.log('files: ', data);
-      }).catch((err) => console.error('Err:', err));
-    }).catch((err) => console.error('Err:', err));
-  });
+  const [data, loading] = useRequest(
+    async () => {
+      try {
+        const repo = await getRepo("balena-io-modules", "rendition");
+        await cloneRepo(repo.data.clone_url);
+        return await getRepoInfo();
+      } catch (err) {
+        notifications.addNotification({
+          content: (err as Error).message,
+          type: "danger",
+        });
+      }
+    },
+    [],
+    { polling: false }
+  );
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+  console.log("*** data", data);
+
+  return (
+    <Box width="100vw" height="100vh">
+      <Spinner show={loading || !data} width="100%" height="100%">
+        {!!data && (
+          <ResponsiveNetwork<NodeProps, LinkProps>
+            data={{
+              nodes: data.data.nodes,
+              links: data.data.links,
+            }}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            linkDistance={function (e) {
+              return e.distance;
+            }}
+            centeringStrength={0.3}
+            repulsivity={6}
+            nodeSize={function (n) {
+              return n.size;
+            }}
+            activeNodeSize={function (n) {
+              return 1.5 * n.size;
+            }}
+            nodeBorderWidth={1}
+            nodeBorderColor={{
+              from: "color",
+              modifiers: [["darker", 0.8]],
+            }}
+            linkBlendMode='multiply'
+            motionConfig='wobbly'
+          />
+        )}
+      </Spinner>
+    </Box>
+  );
+};
 
 export default App;
